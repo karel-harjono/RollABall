9d3dd778298ee40dbe33cd9d3af6a2f9e175ecf5
karelharjono <harjono.karel@gmail.com>
2025-01-26 00:03:37 -0800

    added walls and movement scripts

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private float ballSpeed = 2f;
+
+    public void MoveBall(Vector2 input)
+    {
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }    
+}
